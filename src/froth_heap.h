d02a405dc071770f07e67a0cbe4694abe1af98bd
Niko Kozak <nikokozak@gmail.com>
2026-02-28 17:36:42 -0500

    quotation proof, heap accessor helper

--- src/froth_heap.h
+++ src/froth_heap.h
@@ -10,5 +10,14 @@ typedef struct {
   froth_cell_u_t pointer;
 } froth_heap_t;
 
+froth_heap_t froth_heap;
+
+/* Return a froth_cell_t pointer into the heap at the given byte offset.
+ * This is the single sanctioned way to access cell-sized data in the heap.
+ * All byte_offset values must come from froth_heap_allocate_cells. */
+static inline froth_cell_t* froth_heap_cell_ptr(froth_heap_t* heap, froth_cell_u_t byte_offset) {
+  return (froth_cell_t*)&heap->data[byte_offset];
+}
+
 froth_error_t froth_heap_allocate_bytes(froth_cell_u_t size, froth_heap_t* froth_heap, froth_cell_u_t* assigned_heap_location);
 froth_error_t froth_heap_allocate_cells(froth_cell_u_t size, froth_heap_t* froth_heap, froth_cell_u_t* assigned_heap_location);
--- src/main.c
+++ src/main.c
@@ -1,34 +1,46 @@
 #include "froth_types.h"
 #include "froth_stack.h"
 #include "froth_heap.h"
+#include "froth_slot_table.h"
 #include <stdio.h>
 
 int main() {
-  
-  froth_stack_push(&froth_ds_stack, 42);
-  froth_stack_push(&froth_ds_stack, 69);
-  froth_stack_push(&froth_ds_stack, 1337);
 
-  froth_cell_t value;
+  // Hand encoding "[ 1 2 ]"
 
-  froth_stack_pop(&froth_ds_stack, &value);
-  fprintf(stdout, "Popped value: %" FROTH_CELL_FORMAT "\n", value);
-  froth_stack_pop(&froth_ds_stack, &value);
-  fprintf(stdout, "Popped value: %" FROTH_CELL_FORMAT "\n", value);
-  froth_stack_peek(&froth_ds_stack, &value);
-  fprintf(stdout, "Peeked value: %" FROTH_CELL_FORMAT "\n", value);
-  froth_stack_pop(&froth_ds_stack, &value);
-  fprintf(stdout, "Popped value: %" FROTH_CELL_FORMAT "\n", value);
-  if (froth_stack_pop(&froth_ds_stack, &value) == FROTH_ERROR_STACK_UNDERFLOW) {
-    fprintf(stderr, "Stack underflow error!\n");
+  froth_cell_t quote_ref_cell;
+  froth_cell_u_t quote_ref_raw;
+  froth_heap_allocate_cells((froth_cell_u_t)3, &froth_heap, &quote_ref_raw);
+
+  froth_cell_t* quote_body = froth_heap_cell_ptr(&froth_heap, quote_ref_raw);
+
+  quote_body[0] = 2; // length of quote body
+  froth_make_cell(1, FROTH_NUMBER, &quote_body[1]);
+  froth_make_cell(2, FROTH_NUMBER, &quote_body[2]);
+  froth_make_cell((froth_cell_t)quote_ref_raw, FROTH_QUOTE, &quote_ref_cell);
+
+  // Push the QuoteRef onto the data stack
+  froth_stack_push(&froth_ds_stack, quote_ref_cell);
+
+  // Execute the quotation: pop QuoteRef, walk body, push literals
+  froth_cell_t quote_ref_from_stack;
+
+  froth_stack_pop(&froth_ds_stack, &quote_ref_from_stack);
+  if (FROTH_GET_CELL_TAG(quote_ref_from_stack) == FROTH_QUOTE) {
+    froth_cell_t* quote_start = froth_heap_cell_ptr(&froth_heap, FROTH_STRIP_CELL_TAG(quote_ref_from_stack));
+    froth_cell_t quote_len = quote_start[0];
+
+    for (int i = 0; i < quote_len; i++) {
+      froth_stack_push(&froth_ds_stack, quote_start[i + 1]);
+    }
   }
-  if (froth_stack_peek(&froth_ds_stack, &value) == FROTH_ERROR_STACK_UNDERFLOW) {
-    fprintf(stderr, "Stack underflow error!\n");
+
+  // Verify: should print depth 2, values 1 and 2
+  printf("Stack depth: %" FROTH_CELL_U_FORMAT "\n", froth_stack_depth(&froth_ds_stack));
+  for (int i = 0; i < froth_stack_depth(&froth_ds_stack); i++) {
+    froth_cell_t cell = froth_ds_stack.data[i];
+    printf("  [%d]: %" FROTH_CELL_FORMAT "\n", i, FROTH_STRIP_CELL_TAG(cell));
   }
-
-  while (froth_stack_push(&froth_ds_stack, 0) != FROTH_ERROR_STACK_OVERFLOW) { }
-  fprintf(stderr, "Stack overflow error!\n");
-
 
   return 0;
 }
